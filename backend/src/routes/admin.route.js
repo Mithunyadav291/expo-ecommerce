b27a58e756312916e797a0511741433722f3e78f
Mithunyadav291 <yadavmithun291@gmail.com>
2026-01-08 17:49:48 +0545

    added admin routes and controllers

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,7 +1,12 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
+  getAllCustomers,
+  getAllOrders,
   getAllProduct,
+  getDashboardStats,
+  updateOrderStatus,
   updateProduct,
 } from "../controllers/admin.controller.js";
 import { adminOnly, protectRoute } from "../middleware/auth.middleware.js";
@@ -15,5 +20,16 @@ router.use(protectRoute, adminOnly);
 router.post("/products", upload.array("images", 3), createProduct);
 router.get("/products", getAllProduct);
 router.put("/products/:id", upload.array("images", 3), updateProduct);
+router.delete("/products/:id", deleteProduct);
+
+router.get("/orders", getAllOrders);
+router.patch("/orders/:orderId/status", updateOrderStatus);
+
+router.get("/customers", getAllCustomers);
+
+router.get("/stats", getDashboardStats);
+
+// PUT: Used for full resource replacement, updating the entire resource
+// PATCH: Used for partial resource updates, updating a specific part of the resource
 
 export default router;
