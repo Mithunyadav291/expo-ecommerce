619a89cc777edc0a11dcdefbc1a215db69702db5
Mithunyadav291 <yadavmithun291@gmail.com>
2026-01-10 17:53:04 +0545

    tiny changes

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </div>
+  );
 };
 
 export default LoginPage;
