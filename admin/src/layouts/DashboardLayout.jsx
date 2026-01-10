80c0938e57e99fe5fcd6d741dc112704bfbc22d3
Mithunyadav291 <yadavmithun291@gmail.com>
2026-01-10 17:26:20 +0545

    added sidebar and navbar

--- admin/src/layouts/DashboardLayout.jsx
+++ admin/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
   return (
-    <div>
-      sidebar navbar
-      <Outlet />
+    <div className="drawer lg:drawer-open">
+      <input
+        id="my-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        defaultChecked
+      />
+      <div className="drawer-content">
+        <Navbar />
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+
+      <Sidebar />
     </div>
   );
 };
